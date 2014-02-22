9f24b8ff8e0121c0f875b0863c961714e0f48b13
gshaw <graeme@arcticpepper.com>
2014-02-22 14:51:41 -0600

    Added more vehicles to seed

--- FleetManager/Databases/FleetInitializer.cs
+++ FleetManager/Databases/FleetInitializer.cs
@@ -73,10 +73,22 @@ namespace FleetManager.Databases
 
             var vehicles = new List<Vehicle>
             {
-                new Vehicle{ Reg="G979 TUS", MakeId = 2, Model="Cavalier", Description="Pool car", VehicleTypeId= 1, BodyTypeId= 2, FuelTypeId=1, Base="Harlow", Colour="Red",
-                    EngineSize=1600, TankSize=60, TransmissionId=1, RoadTaxDue="1/5/2014", MotDue="1/5/2014", ServiceDue="1/5/2014", TaxClass="L1" },
-                new Vehicle{ Reg="EO10 BXB", MakeId = 1, Model="Mondeo", Description="Pool car", VehicleTypeId=1, BodyTypeId=3, FuelTypeId=2, Base="Harlow", Colour="Grey",
-                    EngineSize=2000, TankSize=70, TransmissionId=1, RoadTaxDue="1/5/2014", MotDue="1/5/2014", ServiceDue="1/5/2014", TaxClass="L1" },
+                new Vehicle{ Reg="G979 TUS", MakeId = 2, Model="Cavalier", Description="Pool car", 
+                    VehicleTypeId= 1, BodyTypeId= 2, FuelTypeId=1, Base="Harlow", Colour="Red",
+                    EngineSize=1600, TankSize=60, TransmissionId=1, RoadTaxDue="1/5/2014", 
+                    MotDue="1/5/2014", ServiceDue="1/5/2014", TaxClass="L1" },
+                new Vehicle{ Reg="EO10 BXB", MakeId = 1, Model="Mondeo", Description="Pool car", 
+                    VehicleTypeId=1, BodyTypeId=3, FuelTypeId=2, Base="Harlow", Colour="Grey",
+                    EngineSize=2000, TankSize=70, TransmissionId=1, RoadTaxDue="1/5/2014", 
+                    MotDue="1/5/2014", ServiceDue="1/5/2014", TaxClass="L1" },
+                new Vehicle{ Reg="T623 WGK", MakeId = 3, Model="ForTwo", Description="Pool car", 
+                    VehicleTypeId=1, BodyTypeId=1, FuelTypeId=1, Base="Epping", Colour="Black/Grey",
+                    EngineSize=700, TankSize=20, TransmissionId=3, RoadTaxDue="1/5/2014", 
+                    MotDue="1/5/2014", ServiceDue="1/5/2014", TaxClass="L1" },
+                new Vehicle{ Reg="S642 AEC", MakeId = 4, Model="A3", Description="Pool car", 
+                    VehicleTypeId=1, BodyTypeId=5, FuelTypeId=1, Base="Epping", Colour="Silver",
+                    EngineSize=1800, TankSize=70, TransmissionId=1, RoadTaxDue="1/5/2014", 
+                    MotDue="1/5/2014", ServiceDue="1/5/2014", TaxClass="L1" },
             };
             vehicles.ForEach(v => context.vehicles.Add(v));
             context.SaveChanges();
