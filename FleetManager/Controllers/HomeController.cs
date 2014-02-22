06edec534136c5fac6d9c54bfabd40df528ed489
gshaw <graeme@arcticpepper.com>
2014-02-22 15:14:18 -0600

    added two col layout and vehicles to home page

--- FleetManager/Controllers/HomeController.cs
+++ FleetManager/Controllers/HomeController.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FleetManager.Models;
+using FleetManager.Databases;
 
 namespace FleetManager.Controllers
 {
     public class HomeController : Controller
     {
+        private FleetContext db = new FleetContext();
+
         public ActionResult Index()
         {
             ViewBag.Message = "Fleet Manager";
 
-            return View();
+            return View(db.vehicles.ToList());
         }
 
         
